0d5a3b5a17e83ea701c2a881f93af29b925bd0b1
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2019-02-22 06:01:10 +0000

    mockup done

--- traverse.py
+++ traverse.py
@@ -2,6 +2,8 @@ import sys
 import gdb
 import graphviz
 
+from graphviz import Digraph
+
 def is_container(v):
     c = v.type.code
     return (c == gdb.TYPE_CODE_STRUCT or c == gdb.TYPE_CODE_UNION)
@@ -13,24 +15,106 @@ program_name = sys.argv[0]
 arguments = sys.argv[1:]
 count = len(arguments)
 
+def make_pt_node_type_enum():
+    enum_typedef = gdb.lookup_type("PT_NODE_TYPE")
+    gdb.write("%s\n" % enum_typedef.code)
+    node_type = gdb.types.get_basic_type(enum_typedef)
+    return gdb.types.make_enum_dict (node_type)
+
+def is_pt_node(v):
+    pt_node_type = gdb.lookup_type("PT_NODE")
+    return (v.type.__str__() == pt_node_type.__str__())
+
 def cpt_parser(s):
-    if is_container(s) or is_pointer(s) :
-        gdb.write("%s\n" % "container");
+    create_pt_node(s)
+
+def gdb_write(s):
+    gdb.write("%s\n" % s)
+
+def init_dot():
+    global dot
+    global node_cnt
+    
+    dot = Digraph()
+    dot.body.append("newrank=true")
+    node_cnt = 0
+
+def init_cub_types():
+    global type_pt_node
+    global type_pt_nodetype
+    global type_pt_statement_info
+
+    type_pt_node = gdb.lookup_type("PT_NODE")
+    type_pt_nodetype = gdb.lookup_type("PT_NODE_TYPE")
+    type_pt_statement_info = gdb.lookup_type("PT_STATEMENT_INFO")
+
+
+def create_node_attr(v):
+    attr = {}
+    attr['shape'] = 'record'
+    attr['label'] = v.type.__str__()
+    return attr
+
+def attr_to_str(attr):
+    return ' '.join(['%s=%s' % (key, value) for (key, value) in attr.items()])
+
+def create_pt_node(s):
+    node_s = add_brackets(s)
+    this_v = eval_str(node_s)
+
+    attr = create_node_attr(this_v)
+    id = add_dot_node(attr)
+
+    node_type_str = node_s + ".node_type"
+    node_type = eval_str(node_type_str)
+    gdb_write(node_type)
+
+    info_str = node_s + ".info"
+    info_id = create_info_node(info_str)
+    
+    add_dot_edge(id, info_id, 'a')
+    gdb_write(dot.source)
+   
+def create_info_node(s):
+    global node_cnt
+
+    info_s = add_brackets(s)
+    this_v = eval_str(info_s)
+    attr = create_node_attr(this_v)
+    id = add_dot_node(attr)
+
+    #
+    return id 
+
+def add_dot_node(attr):
+    global node_cnt
+    dot.node(str(node_cnt), attr_to_str(attr))
+    node_cnt += 1
+    return node_cnt - 1
+
+def add_dot_edge(f, to, attr):
+    dot.edge(str(f), str(to))
+
+def eval_str(s):
+    return gdb.parse_and_eval(s)
+
+def add_brackets(s):
+    return "(" + s + ")"
+
     
 class CUBRID_PTNODE_Traversal(gdb.Command):
     '''
-    print-struct-follow-pointers [/LEVEL_LIMIT] STRUCT-VALUE
+    cpt_parser PT_NODE
     '''
     def __init__(self): 
         super(CUBRID_PTNODE_Traversal, self).__init__(
             'cpt_parser',
             gdb.COMMAND_DATA, gdb.COMPLETE_SYMBOL, False)
+        
+        init_dot()
+        init_cub_types()
 
     def invoke(self, arg, from_tty):
-        try:
-            v = gdb.parse_and_eval(arg)
-        except gdb.error, e:
-            raise gdb.GdbError(e.message)
-        cpt_parser(v)
+        cpt_parser(arg)
 
 CUBRID_PTNODE_Traversal()
